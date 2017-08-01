2768d46c01f73bae2f1bf69b5b2e3c729f96e17c
Hack Reactor Student <student@hackreactor.com>
2017-07-31 22:46:46 -0700

    Did more, not sure if it was really useful.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,8 +1,26 @@
 const models = require('../models');
 const Promise = require('bluebird');
+const cookieParser = require('./cookieParser');
 
 module.exports.createSession = (req, res, next) => {
+  if (Object.keys(req.cookies).length === 0) {
+    models.Sessions.create()
+    .then((session) => { 
+      var hashId = session.insertId;
+      let query = {id : hashId};
+      models.Sessions.get(query)
+      .then((searchReturn) => {
+        req.session = {hash: searchReturn.hash};
+      });
+    })
+    .catch((error) => {
+      console.log('ERROR');
+      res.status(500).send();
+    });
+  }
 };
+
+
 
 /************************************************************/
 // Add additional authentication middleware functions below
