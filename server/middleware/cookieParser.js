8a019a433ca8e0607ed70f9a220f55ad2ec8ad52
Hack Reactor Student <student@hackreactor.com>
2017-07-31 21:04:00 -0700

    Pass cookie parser tests

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,14 @@
 const parseCookies = (req, res, next) => {
+  var cookies = req.headers.cookie;
+  if (!cookies) {
+    return {};
+  }
+  cookiesArr = cookies.split('; ').map(cookie => cookie.split('='));
+  var obj = {};
+  cookiesArr.forEach(cookie => obj[cookie[0]] = cookie[1]);
+  req.cookies = obj;
+  next();
 };
 
 module.exports = parseCookies;
+
